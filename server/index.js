7918e1b1fe5532700753993ec26495678ffbd855
E-Waste <ewaste@e-wastes-mbp.softlayer.local>
2022-11-15 18:07:48 -0800

    Creating new branch

--- server/index.js
+++ server/index.js
@@ -3,6 +3,7 @@ import cors from "cors"
 import mongoose from "mongoose";
 import bodyParser from "body-parser"
 import dotenv from 'dotenv'
+import multer from "multer";
 
 import posts from './routes/posts.js'
 
@@ -10,21 +11,23 @@ const app = express()
 app.use(cors())
 dotenv.config()
 
-app.use(bodyParser.json({limit: '30mb', extended: true}))
-app.use(bodyParser.urlencoded({limit: '30mb', extended: true}))
+app.use(bodyParser.json({ limit: '30mb', extended: true }))
+app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }))
+
+const upload = multer({ dest: './public/data/uploads/' })
 
 app.use('/posts', posts)
 
 // Environment variables here
-const secret =  process.env.SECRET || "ThisShouldBeABetterSecret"
+const secret = process.env.SECRET || "ThisShouldBeABetterSecret"
 const DB_URL = process.env.DB_URL || "mongodb://localhost:27017/Memories"
-const PORT = process.env.PORT || 5000 
+const PORT = process.env.PORT || 5000
 
 /*
     Connect to database and listen for requests here
     Currently using local MongoBD, switch to the cloud version for deployment
 */
-mongoose.connect(DB_URL, {useNewUrlParser: true, useUnifiedTopology: true})
+mongoose.connect(DB_URL, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => app.listen(PORT, () => console.log(`Server listening on port ${PORT}`)))
     .catch((error) => console.log(error.message))
 
