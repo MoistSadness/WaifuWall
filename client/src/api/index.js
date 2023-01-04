26e72cf410f8a8e2fc3248c5faf1d0ec105b2265
E-Waste <ewaste@e-wastes-mbp.softlayer.local>
2023-01-03 18:06:24 -0800

    modifying environment variable stuff

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,6 +1,7 @@
 import axios from 'axios'
 
-const url = 'http://localhost:5000/posts'
+const url = process.env.REACT_APP_URL
+console.log(url)
 
 export const fetchPosts = () => axios.get(url)
 export const getPost = (id) => axios.get(`${url}/${id}`)
